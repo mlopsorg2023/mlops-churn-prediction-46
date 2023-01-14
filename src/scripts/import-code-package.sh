776b608db799169d116bd148379e473b5e85f96b
Frank Ketelaars <fketelaars@nl.ibm.com>
2023-01-14 09:59:51 +0000

    Update import script

--- src/scripts/import-code-package.sh
+++ src/scripts/import-code-package.sh
@@ -1,34 +1,33 @@
 #! /bin/bash
-export PATH=$PATH/bin
+export PATH=$PATH:$HOME/bin
 
-BASE_SPACE_NAME=mlops-churn-prediction
+BASE_REPO_NAME=$(basename ${REPO_NAME})
+BASE_SPACE_NAME=${BASE_REPO_NAME}
 ENVIRONMENT_NAME="Runtime 22.2 on Python 3.10"
 
-CODE_PACKAGE_NAME=mlops-churn-prediction-46
+CODE_PACKAGE_NAME=${BASE_REPO_NAME}
 NOTEBOOK="src/train-churn-prediction.ipynb"
 
 JOB_DESCRIPTION="Train churn prediction"
 JOB_NAME="train-churn-prediction-job"
 
 # Delete old artifacts
-
 rm -f /tmp/${CODE_PACKAGE_NAME}.zip
-pushd ~/${CODE_PACKAGE_NAME} > /dev/null
-zip -r /tmp/${CODE_PACKAGE_NAME}.zip *
-popd > /dev/null
 
-TS=$(date +"%Y%m%d%H%M%S")
-space_name=${BASE_SPACE_NAME}-${TS}
+# Create zip file from current directory
+zip -r /tmp/${CODE_PACKAGE_NAME}.zip *
 
 # Create space
+TS=$(date +"%Y%m%d%H%M%S")
+space_name=${BASE_SPACE_NAME}-${CURRENT_BRANCH}-${TS}
 echo "Creating space ${space_name}"
 space_id=$(cpdctl space create --name ${space_name} --output json --raw-output --jmes-query "metadata.id")
-echo "The new space is $SPACE_NAME, id $space_id"
+echo "Space ${space_name} created with id ${space_id}"
 
 # Create code package in space
 echo "Creating code package ${CODE_PACKAGE_NAME} in space ${space_name}"
 cpdctl asset file upload --path code_package/${CODE_PACKAGE_NAME} \
-    --file /tmp/${CODE_PACKAGE_NAME}.zip \
+    --file /tmp/${BASE_REPO_NAME}.zip \
     --space-id ${space_id}
 
 code_package_id=$(cpdctl code-package create \
@@ -53,6 +52,7 @@ job_json='{
     "name": "%s"
     }\n'
 job_submit_json=$(printf "${job_json}" "${code_package_id}" "${environment_id}" "${CURRENT_BRANCH}" "${NOTEBOOK}" "${JOB_DESCRIPTION}" "${JOB_NAME}")
+echo "Job submit JSON: ${job_submit_json}"
 
 job_id=$(cpdctl job create --job "${job_submit_json}" --space-id ${space_id} --output json -j "metadata.asset_id" --raw-output)
 
