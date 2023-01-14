9c03bfc7006475ff3d4110f95fdabe34c43fdb3b
Frank Ketelaars <fketelaars@nl.ibm.com>
2023-01-14 09:34:27 +0000

    Test deployment

--- src/scripts/import-code-package.sh
+++ src/scripts/import-code-package.sh
@@ -1,42 +1,67 @@
 #! /bin/bash
 export PATH=$PATH/bin
 
-# Delete old artifacts
+BASE_SPACE_NAME=mlops-churn-prediction
+ENVIRONMENT_NAME="Runtime 22.2 on Python 3.10"
+
 CODE_PACKAGE_NAME=mlops-churn-prediction-46
+NOTEBOOK="src/train-churn-prediction.ipynb"
+
+JOB_DESCRIPTION="Train churn prediction"
+JOB_NAME="train-churn-prediction-job"
+
+# Delete old artifacts
+
 rm -f /tmp/${CODE_PACKAGE_NAME}.zip
 pushd ~/${CODE_PACKAGE_NAME} > /dev/null
 zip -r /tmp/${CODE_PACKAGE_NAME}.zip *
 popd > /dev/null
 
-BASE_SPACE_NAME=mlops-churn-prediction
 TS=$(date +"%Y%m%d%H%M%S")
-SPACE_NAME=${BASE_SPACE_NAME}-${TS}
+space_name=${BASE_SPACE_NAME}-${TS}
 
 # Create space
-echo "Creating space ${SPACE_NAME}"
-SPACE_ID=$(cpdctl space create --name ${SPACE_NAME} --output json --raw-output --jmes-query "metadata.id")
-echo "The new space is $SPACE_NAME, id $SPACE_ID"
+echo "Creating space ${space_name}"
+space_id=$(cpdctl space create --name ${space_name} --output json --raw-output --jmes-query "metadata.id")
+echo "The new space is $SPACE_NAME, id $space_id"
 
 # Create code package in space
-echo "Creating code package ${CODE_PACKAGE_NAME} in space ${SPACE_NAME}"
+echo "Creating code package ${CODE_PACKAGE_NAME} in space ${space_name}"
 cpdctl asset file upload --path code_package/${CODE_PACKAGE_NAME} \
     --file /tmp/${CODE_PACKAGE_NAME}.zip \
-    --space-id ${SPACE_ID}
+    --space-id ${space_id}
 
-CODE_PACKAGE_ID=$(cpdctl code-package create \
+code_package_id=$(cpdctl code-package create \
     --file-reference code_package/${CODE_PACKAGE_NAME} \
     --name ${CODE_PACKAGE_NAME} \
-    --space-id ${SPACE_ID} \
+    --space-id ${space_id} \
     --output json -j "metadata.asset_id" --raw-output)
 
 # Create job
-environment_name="Runtime 22.2 on Python 3.10"
-query_string="(resources[?entity.environment.display_name == '${environment_name}'].metadata.asset_id)[0]"
-ENVIRONMENT_ID=$(cpdctl environment list --space-id ${SPACE_ID} --output json -j "${query_string}" --raw-output)
+echo "Creating job ${JOB_NAME} with environment ${ENVIRONMENT_NAME}"
+query_string="(resources[?entity.environment.display_name == '${ENVIRONMENT_NAME}'].metadata.asset_id)[0]"
+environment_id=$(cpdctl environment list --space-id ${space_id} --output json -j "${query_string}" --raw-output)
 
-job_name="train-churn-prediction-job
-job=''
-job+='{"asset_ref": "'
-job+="${CODE_PACKAGE_ID}"
-job+='"}'
-echo $job | jq .
+job_json='{
+    "asset_ref": "%s", 
+    "configuration": 
+        {
+            "env_id": "%s", 
+            "env_variables": ["CURRENT_BRANCH=%s"], 
+            "entrypoint": "%s" }, 
+    "description": "%s", 
+    "name": "%s"
+    }\n'
+job_submit_json=$(printf "${job_json}" "${code_package_id}" "${environment_id}" "${CURRENT_BRANCH}" "${NOTEBOOK}" "${JOB_DESCRIPTION}" "${JOB_NAME}")
+
+job_id=$(cpdctl job create --job "${job_submit_json}" --space-id ${space_id} --output json -j "metadata.asset_id" --raw-output)
+
+# Run job
+echo "Running job"
+job_run_id=$(cpdctl job run create --space-id ${space_id} --job-id ${job_id} --job-run '{}' --output json -j "metadata.asset_id" --raw-output)
+echo "Job run id: ${job_run_id}"
+
+# Delete temporary space
+cpdctl space delete --space-id ${space_id}
+
+echo "Finished"
